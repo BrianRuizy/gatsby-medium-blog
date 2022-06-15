0227dd460b0d352c20bde96a2993e8c46a209688
Brian Ruiz <brianruiz123@yahoo.com>
2022-06-15 00:38:36 -0500

    update mdxprovider components

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,10 +2,118 @@ import * as React from "react"
 import { Link, graphql } from "gatsby"
 
 import { MDXRenderer } from "gatsby-plugin-mdx"
+import { MDXProvider } from "@mdx-js/react"
+// import { shortcodes } from "../components/shortcodes"
 
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+
+
+import Typography from "@mui/material/Typography"
+import Divider from "@mui/material/Divider"
+
+const shortcodes = {
+  p: (props) => (
+    <Typography
+      variant="body1"
+      sx={{
+        fontFamily: "Charter, serif",
+        fontSize: "20px",
+        lineHeight: "32px",
+        letterSpacing: "-0.003em",
+        mt: "2rem",
+        "@media (max-width: 900px)": {
+          fontSize: "18px",
+          lineHeight: "30px",
+        },
+      }}
+      {...props}
+    />
+  ),
+  h1: (props) => (
+    <Typography
+      variant="h2"
+      sx={{
+        fontSize: "22px !important",
+        fontWeight: "900",
+        lineHeight: "28px",
+        letterSpacing: "0",
+        mt: "3.14em",
+        mb: -2,
+        "@media (max-width: 900px)": {
+          fontSize: "20px !important",
+        },
+      }}
+      {...props}
+    />
+  ),
+  h2: (props) => (
+    <Typography
+      variant="h3"
+      sx={{
+        fontSize: "20px !important",
+        letterSpacing: "0",
+        mt: "2.37em",
+        mb: -2,
+        fontWeight: "700",
+        "@media (max-width: 900px)": {
+          fontSize: "18px !important",
+        },
+      }}
+      {...props}
+    />
+  ),
+  blockquote: (props) => (
+    <Typography
+      variant="h2"
+      sx={{
+        pl: 2,
+        my: "2rem",
+        color: "text.secondary",
+        lineHeight: "40px",
+        borderLeft: "2px solid ",
+        borderColor: "text.primary",
+        fontStyle: 'italic',
+        lineHeight: '1.58',
+        "& > p": {
+          fontWeight: "400",
+          letterSpacing: "-0.003em",
+          lineHeight: "inherit",
+          m: "unset",
+        },
+        "@media (max-width: 900px)": {
+          mx: 4,
+          "& > p": {
+            fontSize: "20px",
+          },
+        },
+      }}
+      {...props}
+    />
+  ),
+  ul: (props) => ( <ul {...props}  />),
+  li: (props) => (
+    <li
+      {...props}
+      style={{
+        marginTop: '1.14em',
+        marginBottom: '-0.46em',
+        fontFamily: "Charter, serif",
+        fontSize: "20px",
+        lineHeight: "32px",
+        letterSpacing: "-0.003em",
+        "@media (max-width: 900px)": {
+          fontSize: "18px",
+          lineHeight: "30px",
+        },
+      }}
+    />
+  ),
+  hr: (props) => ( <Divider sx={{mt: 6 }}/>),
+  a: (props) => ( <Link {...props} />),
+}
+
 
 const BlogPostTemplate = ({ data, location }) => {
   const post = data.mdx
@@ -40,12 +148,25 @@ const BlogPostTemplate = ({ data, location }) => {
           </div>
         )}
         <header>
-          <h1 itemProp="headline">{post.frontmatter.title}</h1>
+          <Typography
+            variant="h1"
+            sx={{
+              fontSize: "36px",
+              fontWeight: "900",
+              letterSpacing: "-0.016em",
+              lineHeight: "40px",
+              "@media (max-width: 900px)": {
+                fontSize: "28px",
+                lineHeight: "36px",
+              },
+            }}
+           >{post.frontmatter.title}</Typography>
           <p>{post.frontmatter.date}</p>
         </header>
 
-        <MDXRenderer>{post.body}</MDXRenderer>
-
+        <MDXProvider components={shortcodes}>
+          <MDXRenderer>{post.body}</MDXRenderer>
+        </MDXProvider>
         <hr />
         <footer>
           <Bio />
